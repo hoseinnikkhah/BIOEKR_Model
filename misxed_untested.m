560a698903698d5606607463276f6d819a177a4e
Hosein Nikkhah <hosein_nikkhah@chemeng.iust.ac.ir>
2024-08-10 07:28:45 +0000

    Aug 10, 2024, 10:58 AM

--- misxed_untested.m
+++ misxed_untested.m
@@ -245,18 +245,23 @@ bolian = 10^-3;                          % [g/mg]
 c_C = ((c_C_TPH*rho*bolian)/MW);         % [mol/m3]
 
 
+% !!!
 % Sigma refrence               [S/m]
 sigma_0 = ((F^2)*D0*c_0*(tau^2))/(R*T);
 
+% !!!
 % Alpha advection
 alpha = u_0/(c_0*k_0);
 
+% $$$
 % --- Create arrays to save data for export
 x_array = linspace(0,L,nx);
 t_array = linspace(0,tmax,nt);
 
 
 % --- Start of EKR (defualt)
+
+% $$$
 % Flux arrays
 J_HA = zeros(nx,nt);
 J_OH = zeros(nx,nt);
@@ -266,6 +271,7 @@ J_A = zeros(nx,nt);
 J_H = zeros(nx,nt);
 J_C = zeros(nx,nt);
 
+% $$$
 % concentration arrays
 G_HA = zeros(nx,nt);
 G_OH = zeros(nx,nt);
@@ -275,6 +281,7 @@ G_A = zeros(nx,nt);
 G_H = zeros(nx,nt);
 G_C = zeros(nx,nt);
 
+% !!!
 % concentration arrays [remapped]
 G_HA_up = zeros(nx,nt);
 G_OH_up = zeros(nx,nt);
@@ -284,6 +291,7 @@ G_A_up = zeros(nx,nt);
 G_H_up = zeros(nx,nt);
 G_C_up = zeros(nx,nt);
 
+% !!!
 % adsorbed concentration arrays [remapped]
 G_HA_ads = zeros(nx,nt);
 G_OH_ads = zeros(nx,nt);
@@ -293,9 +301,11 @@ G_A_ads = zeros(nx,nt);
 G_H_ads = zeros(nx,nt);
 G_C_ads = zeros(nx,nt);
 
+% !!!
 % Current array
 i_z = zeros(nx, nt);
 
+% !!!
 % Sigma array
 sigma_bar = zeros(nx,nt);
 sum_HA = zeros(nx,nt);
@@ -307,6 +317,7 @@ sum_H = zeros(nx,nt);
 sum_C = zeros(nx,nt);
 sum_total = zeros(nx,nt);
 
+% !!!
 C_HA = zeros(nx,nt);
 C_OH = zeros(nx,nt);
 C_Na = zeros(nx,nt);
@@ -316,6 +327,7 @@ C_H = zeros(nx,nt);
 C_C = zeros(nx,nt);
 C_total = zeros(nx,nt);
 
+% !!!
 % SpeciesConstants
 K_H2O_m = ones(nx,nt);
 K_H2O_m = K_H2O_m*K_H2O;
@@ -326,6 +338,7 @@ K_a_m = K_a_m*K_a;
 K_b_m = ones(nx,nt);
 K_b_m = K_b_m*K_b;
 
+% $$$
 % Rate arrays
 R_HA = zeros(nx,nt);
 R_OH = zeros(nx,nt);
@@ -335,7 +348,8 @@ R_A = zeros(nx,nt);
 R_H = zeros(nx,nt);
 R_C = zeros(nx,nt);
 
-%Adsorbed Rate arrays
+% !!!
+% Adsorbed Rate arrays
 R_HA_ads = zeros(nx,nt);
 R_OH_ads = zeros(nx,nt);
 R_Na_ads = zeros(nx,nt);
@@ -344,6 +358,7 @@ R_A_ads = zeros(nx,nt);
 R_H_ads = zeros(nx,nt);
 R_C_ads = zeros(nx,nt);
 
+% $$$
 % --- Set IC and BC
 G_HA(:,1) = c_0;
 G_OH(:,1) = c_0;
@@ -353,6 +368,7 @@ G_A(:,1) = c_0;
 G_H(:,1) = c_0;
 G_C(:,1) = c_C;
 
+% !!!
 G_HA_up(:,1) = G_HA(:,1)/c_0;
 G_OH_up(:,1) = G_OH(:,1)/c_0;
 G_Na_up(:,1) = G_Na(:,1)/c_Na;
@@ -361,6 +377,7 @@ G_A_up(:,1) = G_A(:,1)/c_0;
 G_H_up(:,1) = G_H(:,1)/c_0;
 G_C_up(:,1) = G_C(:,1)/c_C;
 
+% !!!
 G_HA_ads(:,1) = K_ads*c_0;
 G_OH_ads(:,1) = K_ads*c_0;
 G_Na_ads(:,1) = K_ads*c_Na;
@@ -369,6 +386,7 @@ G_A_ads(:,1) = K_ads*c_0;
 G_H_ads(:,1) = K_ads*c_0;
 G_C_ads(:,1) = K_ads*c_C;
 
+% !!!
 % Fixing alpha advection
 alpha_HA = (D_HA/(tau^2))*10^5;
 alpha_OH = (D_OH/(tau^2))*10^5;
@@ -424,84 +442,6 @@ for m=1:nt-1
     G_C_ads(end,m) = 0;  
 
     for i=2:nx-1
-        % Adsorbed concentration is used for rate of reaction in each term  
-        G_H_ads(i,m+1) = G_H_ads(i,m) + coeff*(alpha_H*((G_H_ads(i+1,m) -2*G_H_ads(i,m) + G_H_ads(i-1,m))*((dt/n)/(dx^2))) - (((u_t_H(i+1,m) - u_t_H(i,m))*((dt/n)/dx)) * ((G_H_ads(i+1,m) - G_H_ads(i,m))*((dt/n)/dx))));
-        G_A_ads(i,m+1) = G_A_ads(i,m) + coeff*(alpha_A*((G_A_ads(i+1,m) -2*G_A_ads(i,m) + G_A_ads(i-1,m))*((dt/n)/(dx^2))) - (((u_t_A(i+1,m) - u_t_A(i,m))*((dt/n)/dx)) * ((G_A_ads(i+1,m) - G_A_ads(i,m))*((dt/n)/dx))));
-        G_C_ads(i,m+1) = G_C_ads(i,m) + coeff*(alpha_C*((G_C_ads(i+1,m) -2*G_C_ads(i,m) + G_C_ads(i-1,m))*((dt/n)/(dx^2))) - (((u_t_C(i+1,m) - u_t_C(i,m))*((dt/n)/dx)) * ((G_C_ads(i+1,m) - G_C_ads(i,m))*((dt/n)/dx))));
-        G_HA_ads(i,m+1) = G_HA_ads(i,m) + coeff*(alpha_HA*((G_HA_ads(i+1,m) -2*G_HA_ads(i,m) + G_HA_ads(i-1,m))*((dt/n)/(dx^2))) - (((u_t_HA(i+1,m) - u_t_HA(i,m))*((dt/n)/dx)) * ((G_HA_ads(i+1,m) - G_HA_ads(i,m))*((dt/n)/dx))));
-        G_Na_ads(i,m+1) = G_Na_ads(i,m) + coeff*(alpha_Na*((G_Na_ads(i+1,m) -2*G_Na_ads(i,m) + G_Na_ads(i-1,m))*((dt/n)/(dx^2))) - (((u_t_Na(i+1,m) - u_t_Na(i,m))*((dt/n)/dx)) * ((G_Na_ads(i+1,m) - G_Na_ads(i,m))*((dt/n)/dx))));
-        G_Cl_ads(i,m+1) = G_Cl_ads(i,m) + coeff*(alpha_Cl*((G_Cl_ads(i+1,m) -2*G_Cl_ads(i,m) + G_Cl_ads(i-1,m))*((dt/n)/(dx^2))) - (((u_t_Cl(i+1,m) - u_t_Cl(i,m))*((dt/n)/dx)) * ((G_Cl_ads(i+1,m) - G_Cl_ads(i,m))*((dt/n)/dx))));
-        G_OH_ads(i,m+1) = G_OH_ads(i,m) + coeff*(alpha_OH*((G_OH_ads(i+1,m) -2*G_OH_ads(i,m) + G_OH_ads(i-1,m))*((dt/n)/(dx^2))) - (((u_t_OH(i+1,m) - u_t_OH(i,m))*((dt/n)/dx)) * ((G_OH_ads(i+1,m) - G_OH_ads(i,m))*((dt/n)/dx))));
-        
-        R_H_ads(i,m+1) = G_H_ads(i,m+1);
-        R_A_ads(i,m+1) = G_A_ads(i,m+1);
-        R_C_ads(i,m+1) = G_C_ads(i,m+1);
-        R_HA_ads(i,m+1) = G_HA_ads(i,m+1);
-        R_Na_ads(i,m+1) = G_Na_ads(i,m+1);
-        R_Cl_ads(i,m+1) = G_Cl_ads(i,m+1);
-        R_OH_ads(i,m+1) = G_OH_ads(i,m+1);
-
-        % the 10^5 is a fixing factor in H and can be different in other Species
-        R_H(i,m+1) = R_H_ads(i,m+1)/((c_0*k_0)*10000);
-        R_A(i,m+1) = R_A_ads(i,m+1)/((c_0*k_0)*10000);
-        R_C(i,m+1) = R_C_ads(i,m+1)/((c_C*k_0)*10000);
-        R_HA(i,m+1) = R_HA_ads(i,m+1)/((c_0*k_0)*10000);
-        R_Na(i,m+1) = R_Na_ads(i,m+1)/((c_Na*k_0)*10000);
-        R_Cl(i,m+1) = R_Cl_ads(i,m+1)/((c_Cl*k_0)*10000);
-        R_OH(i,m+1) = R_OH_ads(i,m+1)/((c_0*k_0)*10000);
-        
-        G_HA_up(i,m+1) = G_HA_up(i,m) + (D_HA_less/(Peclet_calculated))*((G_HA_up(i+1,m) -2*G_HA_up(i,m) + G_HA_up(i-1,m))*((t_step/n)/(x_step^2))) - (((u_t_HA_up(i+1,m) - u_t_HA_up(i,m))*((t_step/n)/x_step)) * ((G_HA_up(i+1,m) - G_HA_up(i,m))*((t_step/n)/x_step)));
-        G_Na_up(i,m+1) = G_Na_up(i,m) + (D_Na_less/(Peclet_calculated))*((G_Na_up(i+1,m) -2*G_Na_up(i,m) + G_Na_up(i-1,m))*((t_step/n)/(x_step^2))) - (((u_t_Na_up(i+1,m) - u_t_Na_up(i,m))*((t_step/n)/x_step)) * ((G_Na_up(i+1,m) - G_Na_up(i,m))*((t_step/n)/x_step)));
-        G_Cl_up(i,m+1) = G_Cl_up(i,m) + (D_Cl_less/(Peclet_calculated))*((G_Cl_up(i+1,m) -2*G_Cl_up(i,m) + G_Cl_up(i-1,m))*((t_step/n)/(x_step^2))) - (((u_t_Cl_up(i+1,m) - u_t_Cl_up(i,m))*((t_step/n)/x_step)) * ((G_Cl_up(i+1,m) - G_Cl_up(i,m))*((t_step/n)/x_step)));
-        G_OH_up(i,m+1) = G_OH_up(i,m) + (D_OH_less/(Peclet_calculated))*((G_OH_up(i+1,m) -2*G_OH_up(i,m) + G_OH_up(i-1,m))*((t_step/n)/(x_step^2))) - (((u_t_OH_up(i+1,m) - u_t_OH_up(i,m))*((t_step/n)/x_step)) * ((G_OH_up(i+1,m) - G_OH_up(i,m))*((t_step/n)/x_step)));
-        G_A_up(i,m+1) = G_A_up(i,m) + (D_A_less/(Peclet_calculated))*((G_A_up(i+1,m) -2*G_A_up(i,m) + G_A_up(i-1,m))*((t_step/n)/(x_step^2))) - (((u_t_A_up(i+1,m) - u_t_A_up(i,m))*((t_step/n)/x_step)) * ((G_A_up(i+1,m) - G_A_up(i,m))*((t_step/n)/x_step)));
-        G_H_up(i,m+1) = G_H_up(i,m) + (D_H_less/(Peclet_calculated))*((G_H_up(i+1,m) -2*G_H_up(i,m) + G_H_up(i-1,m))*((t_step/n)/(x_step^2))) - (((u_t_H_up(i+1,m) - u_t_H_up(i,m))*((t_step/n)/x_step)) * ((G_H_up(i+1,m) - G_H_up(i,m))*((t_step/n)/x_step)));        
-        G_C_up(i,m+1) = G_C_up(i,m) + (D_C_less/(Peclet_calculated))*((G_C_up(i+1,m) -2*G_C_up(i,m) + G_C_up(i-1,m))*((t_step/n)/(x_step^2))) - (((u_t_C_up(i+1,m) - u_t_C_up(i,m))*((t_step/n)/x_step)) * ((G_C_up(i+1,m) - G_C_up(i,m))*((t_step/n)/x_step)));
-         
-        
-        sum_H(i,m) = z_H*D_H_less*G_H_up(i,m);
-        sum_A(i,m) = z_A*D_H_less*G_A_up(i,m);
-        sum_C(i,m) = z_C*D_H_less*G_C_up(i,m);
-        sum_HA(i,m) = z_HA*D_H_less*G_HA_up(i,m);
-        sum_Na(i,m) = z_Na*D_H_less*G_Na_up(i,m);
-        sum_Cl(i,m) = z_Cl*D_H_less*G_Cl_up(i,m);
-        sum_OH(i,m) = z_OH*D_H_less*G_OH_up(i,m);
-
-        % Sigma calculations
-        sum_total(i,m) = sum_H(i,m) + sum_A(i,m) + sum_C(i,m) + sum_HA(i,m) + sum_Na(i,m) + sum_Cl(i,m) + sum_OH(i,m);
-        sigma_bar(i,m) = (1/(tau^2))*sum_total(i,m);
-
-        C_H (i,m) = (G_H_up(i+1,m) - G_H_up(i,m))*((t_step/n)/x_step);
-        C_A (i,m) = (G_A_up(i+1,m) - G_A_up(i,m))*((t_step/n)/x_step);
-        C_C (i,m) = (G_C_up(i+1,m) - G_C_up(i,m))*((t_step/n)/x_step);
-        C_HA (i,m) = (G_HA_up(i+1,m) - G_HA_up(i,m))*((t_step/n)/x_step);
-        C_Na (i,m) = (G_Na_up(i+1,m) - G_Na_up(i,m))*((t_step/n)/x_step);
-        C_Cl (i,m) = (G_Cl_up(i+1,m) - G_Cl_up(i,m))*((t_step/n)/x_step);
-        C_OH (i,m) = (G_OH_up(i+1,m) - G_OH_up(i,m))*((t_step/n)/x_step);
-        C_total(i,m) = (D_H_less*z_H*C_H(i,m) + D_A_less*z_A*C_A(i,m) + D_C_less*z_C*C_C(i,m) + D_HA_less*z_H*C_HA(i,m) + D_Na_less*z_Na*C_Na(i,m) + D_Cl_less*z_Cl*C_Cl(i,m) + D_OH_less*z_OH*C_OH(i,m));
-        
-        i_z(i,m) = sigma_bar(i,m)*dphidx(i,m) - (1/Beta_calculated)*(C_total(i,m));
-        
-        R_H(1,m) = (i_z(2,m)/F)*1000;
-        R_OH(end,m) = (i_z(end,m)/F)*1000;
-
-
-        if i == 41
-            R_OH(i,m) = (i_z(2,m)/F)*(Beta/(Peclet*Z)) + 0.1;
-        end
-        if i == 1
-            R_H(i,m) = (i_z(2,m)/F)*(Beta/(Peclet*Z)) + 0.1;
-        end
-
-        for ii=1:nx
-            if m == 1
-                R_H(ii,m) = (i_z(2,m)/F)*(Beta/(Peclet*Z)) + 0.1;
-                R_H(ii,m) = (i_z(2,m)/F)*(Beta/(Peclet*Z)) + 0.1;
-            end
-        end
-
-    end
-end
 
 % Convert concentration to mol/m3
 G_HA_converted = G_HA_up*c_0;
