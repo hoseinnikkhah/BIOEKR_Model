1cff271c6667ff59f2d71dfeb3c5ae5ab741dea7
Hosein Nikkhah <hosein_nikkhah@chemeng.iust.ac.ir>
2024-08-11 14:23:03 +0000

    Aug 11, 2024, 5:53 PM

--- misxed_untested.m
+++ misxed_untested.m
@@ -454,15 +454,14 @@ for m=1:nt-1
 
     for i=2:nx-1
 
-        G_HA(i,m+1) = G_HA(i,m) + ((D_HA*h2)/tau^2)*(G_HA(i+1,m) -2*G_HA(i,m) + G_HA(i-1,m)) - h1*((u_t_HA(i+1,m) - u_t_HA(i,m))*(G_HA(i+1,m) - G_HA(i,m)));
-        G_Na(i,m+1) = G_Na(i,m) + ((D_Na*h2)/tau^2)*(G_Na(i+1,m) -2*G_Na(i,m) + G_Na(i-1,m)) - h1*((u_t_Na(i+1,m) - u_t_Na(i,m))*(G_Na(i+1,m) - G_Na(i,m)));
-        G_Cl(i,m+1) = G_Cl(i,m) + ((D_Cl*h2)/tau^2)*(G_Cl(i+1,m) -2*G_Cl(i,m) + G_Cl(i-1,m)) - h1*((u_t_Cl(i+1,m) - u_t_Cl(i,m))*(G_Cl(i+1,m) - G_Cl(i,m)));
-        G_OH(i,m+1) = G_OH(i,m) + ((D_OH*h2)/tau^2)*(G_OH(i+1,m) -2*G_OH(i,m) + G_OH(i-1,m)) - h1*((u_t_OH(i+1,m) - u_t_OH(i,m))*(G_OH(i+1,m) - G_OH(i,m)));
-        G_A(i,m+1) = G_A(i,m) + ((D_A*h2)/tau^2)*(G_A(i+1,m) -2*G_A(i,m) + G_A(i-1,m)) - h1*((u_t_A(i+1,m) - u_t_A(i,m))*(G_A(i+1,m) - G_A(i,m)));
-       %G_H(i,m+1) = G_H(i,m) + ((D_H*h2)/tau^2)*(G_H(i+1,m) -2*G_H(i,m) + G_H(i-1,m)) - h1*((u_t_H(i+1,m) - u_t_H(i,m))*(G_H(i+1,m) - G_H(i,m)));
-        G_C(i,m+1) = G_C(i,m) + ((D_C*h2)/tau^2)*(G_C(i+1,m) -2*G_C(i,m) + G_C(i-1,m)) - h1*((u_t_C(i+1,m) - u_t_C(i,m))*(G_C(i+1,m) - G_C(i,m)));
+        G_HA(i,m+1) = G_HA(i,m) + ((D_HA_day*h2)/tau^2)*(G_HA(i+1,m) -2*G_HA(i,m) + G_HA(i-1,m)) - h1*((u_t_HA(i+1,m) - u_t_HA(i,m))*(G_HA(i+1,m) - G_HA(i,m)));
+        G_Na(i,m+1) = G_Na(i,m) + ((D_Na_day*h2)/tau^2)*(G_Na(i+1,m) -2*G_Na(i,m) + G_Na(i-1,m)) - h1*((u_t_Na(i+1,m) - u_t_Na(i,m))*(G_Na(i+1,m) - G_Na(i,m)));
+        G_Cl(i,m+1) = G_Cl(i,m) + ((D_Cl_day*h2)/tau^2)*(G_Cl(i+1,m) -2*G_Cl(i,m) + G_Cl(i-1,m)) - h1*((u_t_Cl(i+1,m) - u_t_Cl(i,m))*(G_Cl(i+1,m) - G_Cl(i,m)));
+        G_OH(i,m+1) = G_OH(i,m) + ((D_OH_day*h2)/tau^2)*(G_OH(i+1,m) -2*G_OH(i,m) + G_OH(i-1,m)) - h1*((u_t_OH(i+1,m) - u_t_OH(i,m))*(G_OH(i+1,m) - G_OH(i,m)));
+        G_A(i,m+1) = G_A(i,m) + ((D_A_day*h2)/tau^2)*(G_A(i+1,m) -2*G_A(i,m) + G_A(i-1,m)) - h1*((u_t_A(i+1,m) - u_t_A(i,m))*(G_A(i+1,m) - G_A(i,m)));
+        G_H(i,m+1) = G_H(i,m) + ((D_H_day*h2)/tau^2)*(G_H(i+1,m) -2*G_H(i,m) + G_H(i-1,m)) - h1*((u_t_H(i+1,m) - u_t_H(i,m))*(G_H(i+1,m) - G_H(i,m)));
+        G_C(i,m+1) = G_C(i,m) + ((D_C_day*h2)/tau^2)*(G_C(i+1,m) -2*G_C(i,m) + G_C(i-1,m)) - h1*((u_t_C(i+1,m) - u_t_C(i,m))*(G_C(i+1,m) - G_C(i,m)));
         
-        G_H(i,m+1) = G_H(i,m) + ((D_H_day*h2)/tau^2)*(G_H(i+1,m) -2*G_H(i,m) + G_H(i-1,m)) - h1*((u_t_H_day(i+1,m) - u_t_H_day(i,m))*(G_H(i+1,m) - G_H(i,m)));
 
         G_HA(end,m) = J_HA(i,m);    %--- Lower boundary
         G_OH(end,m) = J_OH(i,m);    %--- Lower boundary
@@ -502,7 +501,7 @@ hold on;
 %plot(t_array,G_A_converted(10,:),'-','DisplayName', 'A-');
 %plot(t_array,G_Na_converted(10,:),'-','DisplayName', 'Na+');
 %plot(t_array,G_Cl_converted(10,:),'-','DisplayName', 'Cl');
-plot(t_array,G_H(10,:),'-','DisplayName', 'H+ ');
+plot(t_array,G_OH(10,:),'-','DisplayName', 'H+ ');
 %plot(t_array,G_OH_converted(10,:),'-','DisplayName', 'OH- ');
 %plot(t_array,G_C_TPH_f(10,:),'-','DisplayName', 'Hydrocarbon');
 
