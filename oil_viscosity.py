38258c0947f065c777180f7b940b1d8527f847e4
Hosein Nikkhah <hosein_nikkhah@chemeng.iust.ac.ir>
2024-09-05 22:40:08 +0000

    Sep 6, 2024, 12:40 AM

--- oil_viscosity.py
+++ oil_viscosity.py
@@ -4,20 +4,10 @@ import numpy as np
 # Data for the plot
 API_gravity = np.array([20, 25, 30, 35, 40, 45, 50])
 viscosity_100F = np.array([60, 20.62, 9.96, 4.5, 2.51, 1.316, 0.91])
-viscosity_150F = np.array([31.62, 10, 3.16, 1, 0.316, 0.1, 0.0316])
-viscosity_200F = np.array([10, 3.16, 1, 0.316, 0.1, 0.0316, 0.01])
-viscosity_250F = np.array([3.16, 1, 0.316, 0.1, 0.0316, 0.01, 0.00316])
-viscosity_300F = np.array([1, 0.316, 0.1, 0.0316, 0.01, 0.00316, 0.001])
 
-# Create the plot
 plt.figure()
 
-# Plot each temperature line
 plt.plot(API_gravity, viscosity_100F, marker='s', label='T=100F', color='black')
-plt.plot(API_gravity, viscosity_150F, marker='v', label='T=150F', color='black')
-plt.plot(API_gravity, viscosity_200F, marker='^', label='T=200F', color='black')
-plt.plot(API_gravity, viscosity_250F, marker='o', label='T=250F', color='black')
-plt.plot(API_gravity, viscosity_300F, marker='s', label='T=300F', color='white', markerfacecolor='white')
 
 # Logarithmic scale for y-axis
 plt.yscale('log')
@@ -26,12 +16,8 @@ plt.yscale('log')
 plt.xlabel('Oil API gravity')
 plt.ylabel('Dead oil viscosity, Cp')
 
-# Grid
 plt.grid(True, which="both", ls="--", linewidth=0.5)
-
-# Add legend
 plt.legend()
-
 # Set y-axis limits
 plt.ylim([0.01, 100])
 
