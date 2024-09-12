3d3e76171f9a25e90c532f7e5fb4984650c2c877
Hosein Nikkhah <hosein_nikkhah@chemeng.iust.ac.ir>
2024-09-12 01:35:44 +0000

    Sep 12, 2024, 5:05 AM

--- tau_porosity.py
+++ tau_porosity.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 # Hypothetical data resembling your plot
 porosity = np.linspace(0.01, 1, 100)  # Porosity from 0 to 1
 # Tortuosity decreases with increasing porosity (with a range up to 8, similar to your plot)
-tortuosity = 8 / (porosity + 0.1)  # A decreasing curve from 8 to near 1, adjusting for visual similarity
+tortuosity = 0.85 / (porosity + 0.1)  # A decreasing curve from 8 to near 1, adjusting for visual similarity
 
 # Plotting the original data as a line
 plt.plot(porosity, tortuosity, label='Original Data', color='black')
