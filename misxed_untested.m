108d6a404f8d8a4b1e65caef6588e053f8b8d01d
Hosein Nikkhah <hosein_nikkhah@chemeng.iust.ac.ir>
2024-09-14 22:59:51 +0000

    Sep 15, 2024, 2:29 AM

--- misxed_untested.m
+++ misxed_untested.m
@@ -16,7 +16,7 @@ Mu_csv = data(1, 3);        % Get the value for viscosity
 n_csv = data(1, 4);         % Get the value for Porosity
 epsilon_csv = data(1, 6);   % Get the value for Relative Permittivity
 tau_csv = data(1,9);        % Get the value for Tortuosity
-growth_csv data(1,10);      % Get the value for Microbial Growth
+growth_csv = data(1,10);    % Get the value for Microbial Growth
 
 % $$$
 % Geomesh info
